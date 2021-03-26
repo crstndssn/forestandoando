05d3fc7f61f6954bc66519bc06ffee885eff7b54
crstndssn <dussan29@gmail.com>
2021-03-25 20:54:49 -0500

    hide-some-logos

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -18,9 +18,9 @@ export default class Navigation extends Component {
                         <Link to="/"><img className="w-12" src={logoMobile} alt="Logo"/> </Link>
                     </div>
                     <div className="w-1/3 flex justify-center items-center">
-                        <Link to="/" className="text-lg font-bold tracking-tight mx-7 md:block xs:hidden">Inicio</Link>
+                        {/* <Link to="/" className="text-lg font-bold tracking-tight mx-7 md:block xs:hidden">Inicio</Link>
                         <Link to="/tienda" className="text-lg font-bold tracking-tight md:mx-7 xs:mx-2">Tienda</Link>
-                        <Link to="/blog" className="text-lg font-bold tracking-tight md:mx-7 xs:mx-2">Blog</Link>
+                        <Link to="/blog" className="text-lg font-bold tracking-tight md:mx-7 xs:mx-2">Blog</Link> */}
                     </div>
                     <div className="md:w-1/3 xs:w-auto flex justify-end items-center">
                         <a href="https://www.facebook.com/forestandoando/" target="_blank" rel="noreferrer"><img className="md:w-8 xs:w-7 md:mx-7 xs:mx-4" src={facebook} alt="Facebook" /></a>
